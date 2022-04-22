0a395b9e7980ea92a7c07571648d310fdc87ab03
Xiaoyu Ding <ding0054@algonquinlive.com>
2022-04-22 02:00:25 -0400

    Completes the assignment

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const $btnGet = document.getElementById('btn-get')
 const $result = document.getElementById('result')
 const $date = document.getElementById('date')
+const $hdImage = document.getElementById('hdimage')
+
+window.addEventListener('DOMContentLoaded', checkLocalStorage);
 
 $btnGet.addEventListener('click', function (e) { 
     e.preventDefault()
@@ -18,14 +21,36 @@ $btnGet.addEventListener('click', function (e) {
 
             //title, date, expl.
             $result.innerHTML = `
-            <img src=${data.url}>
+            <img id="resultImage" src=${data.url}>
             <div>
             <h3>${data.title}</h3>
             <p class="date">${data.date}</p>
             <p>${data.explanation}</p>
-            <button>Save to Favourites</button>
+            <button id="save">Save to Favourites</button>
             </div>
             `
+            function addHiddenClass(e) { 
+                e.classList.add('hidden');
+            }
+
+            function removeHiddenClass(e) { 
+                e.classList.remove('hidden');
+            }
+            const $resultImage = document.getElementById('resultImage')
+
+            $resultImage.addEventListener('click', function () {
+                removeHiddenClass($hdImage)   
+                $hdImage.innerHTML=`<img src=${data.hdurl}>`
+            })
+
+            $hdImage.addEventListener('click', function () {
+                addHiddenClass($hdImage)
+            })
+
+            const $save = document.getElementById('save')
+            $save.addEventListener('click', function () {
+                
+            })
 
         })
         .catch(error => {
